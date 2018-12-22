23488c85ff721d991373091a16335bdf1a91d89f
manojsraogithub <manojsraogithub@gmail.com>
2018-12-22 13:47:18 +0000

    withscript

--- scripts/pm2.sh
+++ scripts/pm2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo su -
-/root/.nvm/versions/node/v8.14.0/bin/pm2 start /dhakatribune/testcodedeploy/app.js
+touch f1
+#/root/.nvm/versions/node/v8.14.0/bin/pm2 start /dhakatribune/testcodedeploy/app.js
 
