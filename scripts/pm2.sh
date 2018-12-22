49f0da893eaf548d3911cb13222931c0ee30fc2e
manojsraogithub <manojsraogithub@gmail.com>
2018-12-22 16:04:10 +0000

    withscript

--- scripts/pm2.sh
+++ scripts/pm2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo su -
 cd /dhakatribune/testcodedeploy/
-source /etc/profile
 pm2 start app.js
 
---
+++ scripts/source.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+echo "export PATH=$PATH:/root/.nvm/versions/node/v8.14.0/lib/node_modules/pm2/bin/" >> /root/.bashrc
+source /root/.bashrc
